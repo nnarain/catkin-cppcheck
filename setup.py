342f77e916e10d4cf1a1d380cfb3a96b31c98766
Natesh Narain <nnaraindev@gmail.com>
2018-12-01 12:22:30 -0500

    Update long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+root = os.path.dirname(os.path.realpath(__file__))
+readme = os.path.join(root, 'README.rst')
 
 setup(
     name='catkin-cppcheck',
-    version='0.0.3',
+    version='0.0.4',
     author='Natesh Narain',
     author_email='nnaraindev@gmail.com',
     description='Run cppcheck on catkin packages',
+    long_description=open(readme).read(),
     license='MIT',
     keywords='catkin ros cppcheck c++',
 
