e3f37f5597e05f3a1b1712374ea36f6ae13aa294
Natesh Narain <nnaraindev@gmail.com>
2018-12-01 16:14:58 -0500

    Get latest git tag for package version

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+from __future__ import print_function
+
 import os
-from setuptools import setup, find_packages
+import shlex
+import subprocess
+from setuptools import setup
 
 root = os.path.dirname(os.path.realpath(__file__))
+
+# Long description from README
 readme = os.path.join(root, 'README.rst')
+
+# Read latest git tag
+try:
+    latest_tag = subprocess.check_output(shlex.split('git describe --tags --abbrev=0'), cwd=root)
+except Exception as e:
+    print('Error getting latest git tag. {}'.format(str(e)))
+    exit(1)
 
 setup(
     name='catkin-cppcheck',
-    version='0.0.4',
+    version=latest_tag,
     author='Natesh Narain',
     author_email='nnaraindev@gmail.com',
     description='Run cppcheck on catkin packages',
@@ -17,7 +30,7 @@ setup(
     package_dir={'': 'src'},
     packages=['catkin_cppcheck'],
 
-    install_requires=['catkin_pkg'],
+    install_requires=['catkin-pkg'],
 
     entry_points= {
         'catkin_tools.commands.catkin.verbs': [
